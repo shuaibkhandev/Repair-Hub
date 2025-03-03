c2365200b0b5710e9775095fa582f2cba580ac58
shuaibkhan_unitedsol <skhan.csit@gmail.com>
2025-03-03 10:26:03 +0500

    ..

--- public/js/dashboard/script.js
+++ public/js/dashboard/script.js
@@ -1,9 +1,12 @@
-// const user = JSON.parse(localStorage.getItem("user"));
-// console.log(user)
-// // Redirect non-admin users
-// if (!user && !user.isAdmin) {
-//   window.location.href = "/";
-// }
+
+
+
+const user = JSON.parse(localStorage.getItem("user"));
+console.log(user)
+// Redirect non-admin users
+if (!user || !user.isAdmin) {
+  window.location.href = "/";
+}
 
 
 let menuicn = document.querySelector(".menuicn");
@@ -68,34 +71,34 @@ const showNoti = () => {
   const clearSearch = document.getElementById("clearSearch");
   let bookingsData = [];
   
-  async function fetchBookings(){
-    try{
-        const response = await fetch("http://localhost:8000/bookings");
-          const bookings = await response.json();
-      bookingsData = bookings;
-      displayBookings(bookings)
-    }
-    catch(error){
-          console.error("Error fetching bookings:", error);
-    }
-  }
+  // async function fetchBookings(){
+  //   try{
+  //       const response = await fetch("http://localhost:8000/bookings");
+  //         const bookings = await response.json();
+  //     bookingsData = bookings;
+  //     displayBookings(bookings)
+  //   }
+  //   catch(error){
+  //         console.error("Error fetching bookings:", error);
+  //   }
+  // }
   
-  function displayBookings(bookings) {
-    const bookingList = document.getElementById("booking-list");
-    bookingList.innerHTML = ""; // Clear previous data
+  // function displayBookings(bookings) {
+  //   const bookingList = document.getElementById("booking-list");
+  //   bookingList.innerHTML = ""; // Clear previous data
   
-    bookings.forEach(booking => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-        <td>${booking.serviceType}</td>
-        <td>${booking.name}</td>
-        <td>${booking.phone}</td>
-        <td>${booking.location}</td>
-        <td class="status">${booking.status || "Pending"}</td>
-      `;
-      bookingList.appendChild(row);
-    });
-  }
+  //   bookings.forEach(booking => {
+  //     const row = document.createElement("tr");
+  //     row.innerHTML = `
+  //       <td>${booking.serviceType}</td>
+  //       <td>${booking.name}</td>
+  //       <td>${booking.phone}</td>
+  //       <td>${booking.location}</td>
+  //       <td class="status">${booking.status || "Pending"}</td>
+  //     `;
+  //     bookingList.appendChild(row);
+  //   });
+  // }
   
   function searchBookings(query){
       if (query.trim() === "") {
